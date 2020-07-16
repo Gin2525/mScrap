7f7aea85dd6fb3a158a4578510867daf04b2f0ad
Gin2525 <gin2798@gmail.com>
2020-07-17 05:07:39 +0900

    main

--- main.go
+++ main.go
@@ -1,85 +1,10 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-
-	"github.com/PuerkitoBio/goquery"
+	"mScraper/mailsender"
+	"mScraper/scraper"
 )
-
-const (
-	// MercariSerchBaseURL means seach page in mercari
-	MercariSerchBaseURL string = "https://www.mercari.com/jp/search/"
-)
-
-// MercariItem means item produced
-type MercariItem struct {
-	productName string
-	price       int
-}
-
-// SearchURL have material to render url
-type SearchURL struct {
-	keyword string
-	queries map[string]string
-}
-
-func buildURLStructure(keyword string, queries map[string]string) *SearchURL {
-	return &SearchURL{keyword, queries}
-}
-
-func (urlStructure SearchURL) renderURL() string {
-	url := MercariSerchBaseURL + "?keyword=" + urlStructure.keyword + "&"
-	for k, v := range urlStructure.queries {
-		url += k + "=" + v + "&"
-	}
-	return url[:len(url)-1]
-}
-
-func fetchMercariItems(urlStructure SearchURL) []MercariItem {
-	url := urlStructure.renderURL()
-	res, _ := http.Get(url)
-	// read
-	buf, _ := ioutil.ReadAll(res.Body)
-	bReader := bytes.NewReader(buf)
-	doc, _ := goquery.NewDocumentFromReader(bReader)
-	mercariItemsSelection := doc.Find(".items-box")
-	return exportItemsFromSelection(mercariItemsSelection)
-}
-
-func exportItemsFromSelection(mercariItemsSelection *goquery.Selection) []MercariItem {
-	mercariItems := make([]MercariItem, mercariItemsSelection.Length())
-
-	mercariItemsSelection.Each(func(idx int, s *goquery.Selection) {
-		// get the product names from the selection
-		pNames := mercariItemsSelection.Find(".items-box-name")
-		pNames.Each(func(idx int, s *goquery.Selection) {
-			mercariItems[idx].productName = s.Text()
-		})
-		// get the prices from the selection.
-		prices := mercariItemsSelection.Find(".items-box-price")
-		prices.Each(func(idx int, s *goquery.Selection) {
-			mercariItems[idx].price = convNum(s.Text())
-		})
-	})
-
-	return mercariItems
-}
-
-func convNum(s string) int {
-	n := 0
-	l := len(s)
-	for i := 0; i < l; i++ {
-		if '0' <= s[i] && s[i] <= '9' {
-			n = n*10 + int(s[i]-'0')
-		} else {
-			continue
-		}
-	}
-	return n
-}
 
 func main() {
 	keyword := "十三機兵防衛圏"
@@ -88,9 +13,27 @@ func main() {
 		"category_root":                 "5",
 		"category_child":                "76",
 		"category_grand_child%5B702%5D": "1"}
-	urlStructure := SearchURL{keyword, queries}
-	items := fetchMercariItems(urlStructure)
-	for _,item := range items{
-		fmt.Println(item)
+
+	urlStructure := scraper.SearchURL{Keyword: keyword, Queries: queries}
+	items := scraper.FetchMercariItems(urlStructure)
+
+	msg := urlStructure.RenderURL() + "\n\n"
+	for _, item := range items {
+		msg += item.String()
+	}
+
+	mail := new(mailsender.Mail)
+	mail.From = "username@gmail.com"
+	mail.Username = "username@gmail.com"
+	mail.Password = "password"
+	mail.To = "email address to send"
+	mail.Sub = "title"
+	mail.Msg = msg
+
+	if err := mailsender.SendGmail(*mail); err != nil {
+		fmt.Println(err)
+		fmt.Println("failure...")
+	} else {
+		fmt.Println("success!")
 	}
 }
